042eac1db6f83e75c23c8fe68f062950c571d4f7
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-11 01:57:19 +0300

    Print statement to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints statement to stderr using fwrite
@@ -9,6 +10,6 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(str , 1 , sizeof(str));
+	write(2, str , sizeof(str));
 	return (1);
 }
