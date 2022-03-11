a1b4ecdb8d85b5d4aede75f5f42809126a3df1aa
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-11 22:54:21 +0300

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints decimal numbers 0 - 9
+ *
+ * Return: 0 as success value
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
