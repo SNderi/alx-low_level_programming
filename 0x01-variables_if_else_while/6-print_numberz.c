d3cf0f8f0a1cb7e0e84f7c3a7fb05fcf7bee07e2
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-12 00:05:21 +0300

    Print decimal numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print decimal numbers from 0 to 9
+ *
+ * Return: 0 as success value
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
