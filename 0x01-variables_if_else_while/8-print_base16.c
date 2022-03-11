b95656d4702685c7566f43dbc40e18db5bccf295
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-12 00:47:14 +0300

    Print the lowercase hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers in base 16
+ *
+ * Return: 0 as success value
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a' ; c <= 'f' c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
