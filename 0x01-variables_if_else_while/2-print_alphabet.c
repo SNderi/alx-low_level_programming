0cccbf16118654b27fa1abf33228687cb12df16c
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-11 21:55:17 +0300

    Print the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,18 @@
 /**
  * main - Prints the alphabet set in lowercase
  *
+ * Return: 0 as success value
  */
 
 int main(void)
 {
 	char l;
+	
 	for (l = 'a' ; l <= 'z' ; l++)
 	{
 		putchar(l);
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
 
