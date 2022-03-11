e266bccdf17a3ba2adf6a2d1a76965ee50d0613d
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-12 01:12:36 +0300

    Print numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i <= 8 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		putchar(', ');
 	}
+	putchar(9 + '0');
 	putchar('\n');
 	return (0);
 }
