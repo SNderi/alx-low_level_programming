3519bca7100b997c17e326f8f070d2290a758f54
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-12 23:19:13 +0300

    Print decimal numbers in threes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints  3 combination decimal numbers
+ *
+ * Return: 0 as success value
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i <=9 ; i++)
+	{
+		for (j = i + 1 ; j <= 9 ; j++)
+		{
+			for (k = j + 1 ; k <= 9 ; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				for  (i+j+k != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
