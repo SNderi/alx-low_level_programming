8a00b4be9bcad6694f18959ecc42c07f261ff138
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 02:25:20 +0300

    Compute absolute value of int

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,11 +6,10 @@
  * _abs - Computes absolute value of an int
  *@n: operand
  *
- * Return: 0 as success value
+ * Return: computed value
  */
 
 int _abs(int n)
 {
-	printf("%d", abs(n));
-	return (0);
+	return (abs(n));
 }
