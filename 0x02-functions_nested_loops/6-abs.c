13cf2a52850e3d42230dd3e9a6828745e1a8f556
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 02:23:25 +0300

    Compute absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Computes absolute value of an int
+ *@n: operand
+ *
+ * Return: 0 as success value
+ */
+
+int _abs(int n)
+{
+	printf("%d", abs(n));
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
 int print_sign(int n);
+int _abs(int);
