e9050b4a2bc7af3109bbda4a2451472dccdecb0a
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-15 23:56:58 +0300

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and prints the result
+ *@res: total of summation
+ *
+ * Return: 0 as success value
+ */
+
+int add(int, int)
+{
+	int res;
+
+	res = int + int;
+	printf("%d", res);
+	return (0);
+}
