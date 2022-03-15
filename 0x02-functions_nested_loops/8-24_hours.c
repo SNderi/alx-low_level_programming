deaefbc01d97cffca96497de620b5a38b5933310
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 01:36:59 +0300

    Print minutes of day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 24)
+	{
+		int j = 0;
+
+		while (j < 59)
+		{
+			if (i < 10 $$ j < 10)
+				printf("%d%d:%d%d\n", 0, i, 0, j);
+			elif (j < 10)
+				printf("%d:%d%d\n", i, 0, j);
+			else
+				printf("%d:%d\n", i, j);
+		}
+		j++;
+	}
+	i++;
+}
