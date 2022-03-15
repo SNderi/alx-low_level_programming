282d1ec1253ca7b3fa52531723763538386fcc99
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-15 20:50:10 +0300

    Print _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print _putchar followed by newline
+ *
+ * Return: 0 as success value
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
