6930357e64e0eb3861f3186047151c14a33e0c99
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 17:17:09 +0300

    Print n*n multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,10 +42,10 @@ void print_times_table(int n)
 			else if ((res < 10) && (j != 0))
 			{
 				_putchar(' ');
+				_putchar(' ');
 				_putchar((res % 10) + '0');
 			}
 		}
-		_putchar(' ');
 		_putchar(' ');
 		_putchar('\n');
 	}
