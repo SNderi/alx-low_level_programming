ec45659621dc56c55702ea5fdfa495d93964d3fe
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 17:39:35 +0300

    Print n*n multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,43 +10,37 @@ void print_times_table(int n)
 {
 	int i, j, res;
 
-	for (i = 0; i <= n; i++)
+	if ((n > 15) && (n < 0))
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			res = i * j;
-			if ((n > 15) || (n < 0))
-			{
-				break;
-			}
-			if (j != 0)
+			_putchar('0');
+			for (j = 0; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
+				res = i * j;
+				if (res < 100)
+				{
+					_putchar(' ');
+				}
+				if (res < 10)
+				{
+					_putchar(' ');
+				}
+				if ((res >= 10) && (res < 100))
+				{
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10) % 10) + '0');
+					_putchar((res % 10) + '0');
+				}
 			}
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else if ((res >= 10) && (res < 100))
-			{
-				_putchar(' ');
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if (res >= 100)
-			{
-				_putchar((res / 100) + '0');
-				_putchar(((res / 10) % 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if ((res < 10) && (j != 0))
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((res % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
