30b435fc0d90bc6fd5c33fc989ab8e5b2155d9b1
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-16 17:49:33 +0300

    Print n*n multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,27 +14,34 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
 			for (j = 0; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
 				res = i * j;
-				if (res < 100)
+				if (res == 0)
 				{
-					_putchar(' ');
+					_putchar(res + '0');
 				}
 				if (res < 10)
 				{
+					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
 				}
 				if ((res >= 10) && (res < 100))
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
 				}
 				if (res >= 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((res / 100) + '0');
 					_putchar(((res / 10) % 10) + '0');
 					_putchar((res % 10) + '0');
