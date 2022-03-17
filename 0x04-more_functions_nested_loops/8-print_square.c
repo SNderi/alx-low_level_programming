cb30f09003b481e84f5d2024fd7ef4b241d9d602
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-17 17:49:45 +0300

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square using #
+ * @size: Sides length
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size, i++)
+	{
+		for (j = 1, j <= size, j++)
+		{
+			putchar('#');
+		}
+		if (size < 1)
+		{
+			break;
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
