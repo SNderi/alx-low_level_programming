e29a60f324527cafa96ef51766c852d805610430
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-17 18:08:33 +0300

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * fizzbuzz - Prints 1 to 100 and incorporates fizzbuzz test
+ */
+
+void fizzbuzz(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
