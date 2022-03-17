84b34139ac207c97af77243f293b8067b92aeac4
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-17 12:56:53 +0300

    Print set digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints set digit
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0, i < 10, i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
