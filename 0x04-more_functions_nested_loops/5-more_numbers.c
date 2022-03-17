aa4a658a70e04a152c0a3acb11b727045ceb4b33
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-17 23:09:34 +0300

    Edit on putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			putchar(j + '0');
+			_putchar(j + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-	putchar('\n');
+	_putchar('\n');
 }
