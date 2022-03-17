2183d3cf38ea6c61b3fed77404e79461ef9dc634
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-18 00:26:18 +0300

    Fizzbuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,31 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * fizzbuzz - Prints 1 to 100 and incorporates fizzbuzz test
+ * main - Fizz Buzz Test
+ *
+ * Description: prints the numbers from 1 to 100, followed by a new line
+ * multiples of 3 and 5 are replaced with Fizz and Buzz respectively
+ * multiples of both 3 and 5 are replaced with FizzBuzz
+ *
+ * Return: 0
  */
 
-int fizzbuzz(void)
+int main(void)
 {
-	int i = 1;
+	int i;
 
 	for (i = 1; i < 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if ((i % 3 != 0) && (i % 5 == 0))
 			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz");
+	putchar('\n');
 	return (0);
 }
