3d8b73ff1a0ec752c1d59a1263cfc74445278fa0
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-21 21:49:10 +0300

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_puts - Prints a string followed by a new line
+ *@str: String to be printed
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
