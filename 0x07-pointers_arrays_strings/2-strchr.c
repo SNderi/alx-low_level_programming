34995abc70516a374be3c3f56406fbb545c5355d
SNderi <79838849+SNderi@users.noreply.github.com>
2022-03-28 18:35:52 +0300

    Locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,21 @@
 
 char *_strchr(char *s, char c)
 {
-	for (;*s != '\0'; s++)
+	int a = 0, b;
+
+	while (s[a])
 	{
-		if (*s == c)
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
 		{
+			s += b;
 			return (s);
 		}
 	}
+
 	return ('\0');
 }
