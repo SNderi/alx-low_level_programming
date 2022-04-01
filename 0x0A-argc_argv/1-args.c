1f867c6260055bc0523bdca3e1d799049245cf0c
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-01 15:41:36 +0300

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
