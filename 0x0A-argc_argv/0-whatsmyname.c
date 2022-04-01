44627707bad8c3a16e962478be058e21ea7e0aa1
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-01 15:16:59 +0300

    Editing on spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,14 @@
 
 /**
  * main - prints its name
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ *
+ * Return - 0 on success
  */
 
 int main(int argc, char **argv)
 {
-  printf("%s\n", argv[argc - 1]);
-  return (0);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
