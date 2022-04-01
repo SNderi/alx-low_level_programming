453b83b0ed684d88e433f34afc411a97d990769a
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-01 17:11:00 +0300

    Edit on betty style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,12 +22,12 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 		if (!(*(argv[i]) >= '0' && *(argv[i]) <= '9'))
-			{
-				printf("Error\n");
-				return (1);
-			}
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) > 0)
