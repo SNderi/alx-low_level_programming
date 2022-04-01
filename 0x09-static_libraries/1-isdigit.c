e0e6cd9ed94716282c2362b66eccbc12a787bf33
SNderi <sharonscarlet8@gmail.com>
2022-04-01 21:46:23 +0300

    Update 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isdigit - Checks if value is a digit
+ *@c: Value to be checked
+ *
+ * Return: Result
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
