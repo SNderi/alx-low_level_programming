fb34717d7332581286bbdf97096b51d6351b2ef6
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-05 16:10:28 +0300

    Create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - Creates an array of chars
+ *
+ * @size: size of array
+ * @c: char to initialize the array
+ *
+ * Return: Null if size is O/if it fails else a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc(size * size_of(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# C - malloc and free
+Learning about automatic and dynamic allocation and how to use malloc, free and valgrind
