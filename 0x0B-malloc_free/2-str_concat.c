c3f60af00bb52805d7435351cf11ad7bba6c0673
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-05 16:44:52 +0300

    Returns a pointer to a concatenated string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] || s2[i]; index++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
 	str = malloc(sizeof(char) * len);
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		str[j++] = s1[i];
 
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2[i]; i++)
 		str[j++] = s2[i];
 
 	return (str);
