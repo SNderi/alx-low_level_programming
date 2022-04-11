b9bcb596e9c1f2f791f4bcb81edba24017cf3e8c
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-11 22:56:35 +0300

    Structures, typedef project, dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ *struct dog - Dog structure.
+ *@name: name of the dog
+ *@age: dog's age
+ *@owner: dog's owner name
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _DOG_ */
