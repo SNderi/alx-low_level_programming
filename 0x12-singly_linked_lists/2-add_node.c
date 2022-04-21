7fb0f902b4fd4a556a134209469f9628c677c787
Sharon Nderi <nderisharonw@gmail.com>
2022-04-21 20:49:26 +0000

    Added a semicolon

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	unsigned int i;
-	char *str_copy = strdup(str)
+	char *str_copy = strdup(str);
 
-	for (i = 0; str[i] != '\0';)
-		i++;
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
 	new_node->str  = str_copy;
 	new_node->len = i;
