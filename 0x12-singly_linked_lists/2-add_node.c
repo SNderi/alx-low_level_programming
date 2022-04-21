f71fafe4aca3a83a8a15448f946cabf4815e91db
Sharon Nderi <nderisharonw@gmail.com>
2022-04-21 20:45:29 +0000

    String copy edit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,11 +12,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	unsigned int i;
+	char *str_copy = strdup(str)
 
 	for (i = 0; str[i] != '\0';)
 		i++;
 
-	new_node->str  = strdup(str);
+	new_node->str  = str_copy;
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
