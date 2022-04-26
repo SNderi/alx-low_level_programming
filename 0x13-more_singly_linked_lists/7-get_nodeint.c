3cfc008829f0e48687eac523869933cc6be0449f
Sharon Nderi <nderisharonw@gmail.com>
2022-04-26 23:56:27 +0000

    Return node at nth position

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (i < index)
 	{
+		if (temp == NULL)
+			return (NULL);
 		temp = head->next;
 		head = temp;
 		i++;
 	}
-	return (0);
+	return (temp);
 }
