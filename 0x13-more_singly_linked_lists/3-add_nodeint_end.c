b5c9b61e4f0902f2e8d0073f0355475d83c0b26b
Sharon Nderi <nderisharonw@gmail.com>
2022-04-26 20:30:14 +0000

    Correction

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,14 +22,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 		*head = new_node;
 
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
+	last = *head;
 
-		last->next = new_node;
-	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
 
 	return (new_node);
 }
