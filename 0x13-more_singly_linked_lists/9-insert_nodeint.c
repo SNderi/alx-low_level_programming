0db0df87cb9ed9f07a34dc7a53130251b1b36a63
Sharon Nderi <nderisharonw@gmail.com>
2022-04-27 00:51:26 +0000

    Insert node at given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,17 +15,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 	int i = 0;
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = (*head)
+		(*head) = new_node;
+		return (new_node);
+	}
 
 	if (i < idx - 1)
 	{
-		temp = head->next;
+		temp = temp->next;
 		i++;
 	}
 	new_node->next = temp->next;
 	temp->next = new_node;
 
-	if (new_node == NULL)
-		return (NULL);
 	return (new_node);
 }
