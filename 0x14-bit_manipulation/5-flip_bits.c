ab7953ed1e7a9d419dd063e2e0916aa4bb281cef
Sharon Nderi <nderisharonw@gmail.com>
2022-04-28 22:33:57 +0000

    Number of bits to flip to create another number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns number of bits to flip to get to another no.
+ * @n: Binary number 1
+ * @m: Binary number 2
+ *
+ * Return: Number of bits to flip
+ */
+
+
