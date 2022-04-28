713a853de9506226167b915fa464e321e3c49319
Sharon Nderi <nderisharonw@gmail.com>
2022-04-28 22:40:33 +0000

    Corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,13 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len = strlen(b);
-	int i = 0;
+	int i;
 	unsigned int sum = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (i < len)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
