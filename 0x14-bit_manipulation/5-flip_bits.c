c9bee9ea45af2be00ba69432ce8099447d33fe06
SNderi <sharonscarlet8@gmail.com>
2022-04-29 01:35:43 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,47 @@
 #include "main.h"
 
+int get_bit(unsigned long int n, unsigned int index);
+
 /**
- * flip_bits - Returns number of bits to flip to get to another no.
- * @n: Binary number 1
- * @m: Binary number 2
+ * flip_bits - returns no of bit to flip to get from n to m
+ * @n: interger (from)
+ * @m: integer (to)
  *
- * Return: Number of bits to flip
+ * Return: number of bits I need to flip
  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i = 0;
+	int count = 0;
+	int index = 0;
 
+	while (i < 64)
+	{
+		if (get_bit(n, index) != get_bit(m, index))
+		{
+			count++;
+		}
+		i++;
+		index++;
+	}
+	return (count);
+}
 
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: input decimal number
+ * @index: index to check
+ *
+ * Return: value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int ans;
+
+	if (index > 64)
+		return (-1);
+
+	ans = (n >> index) & 1;
+
+	return (ans);
+}
