7ac3d4bad22c432d2ec02dccbc8f56668d66ab77
Sharon Nderi <nderisharonw@gmail.com>
2022-04-28 20:45:52 +0000

    Convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary number to an unsigned int
+ * @b: String of 0 and 1
+ *
+ * Return: Converted number or 0 if b is null or
+ * 	   theres any char thats not 0 or 1
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = strlen(b);
+	int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (i < len)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		sum = sum << 1;
+		if (b[i] == '1')
+			sum += 1;
+		i++;
+	}
+	return (sum);
+}
