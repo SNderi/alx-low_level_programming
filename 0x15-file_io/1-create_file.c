9c8ce576cd19869292ed39636727d055787f8bb1
Sharon Nderi <nderisharonw@gmail.com>
2022-05-03 21:20:34 +0000

    Correction on betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
 	int len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
