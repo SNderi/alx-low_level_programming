93eebb449c0d736315b5caed1003b46a9524e380
SNderi <sharonscarlet8@gmail.com>
2022-05-04 03:18:26 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,10 @@ int main(int argc, char **argv)
 
 	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (new == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	old = open(argv[1], O_RDONLY);
 	if (old == -1)
 	{
@@ -42,5 +45,6 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", old), exit(100);
 	if (close(new) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", new), exit(100);
+	free(buf);
 	return (0);
 }
