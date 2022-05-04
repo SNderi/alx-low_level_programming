c419ce54c55727f3c6054531c3898d8b3ca573c5
Sharon Nderi <nderisharonw@gmail.com>
2022-05-04 00:12:11 +0000

    Mimic cp command

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,13 +14,9 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+
 	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (new == 1)
+	if (new == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	old = open(argv[1], O_RDONLY);
 	if (old == -1)
