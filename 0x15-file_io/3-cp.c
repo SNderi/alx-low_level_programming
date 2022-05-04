7bdd837936e2bdd4d2f0097247bf74b56dd3c44c
SNderi <sharonscarlet8@gmail.com>
2022-05-04 03:20:24 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,17 +15,18 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
 
-	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (new == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 	old = open(argv[1], O_RDONLY);
 	if (old == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
+	
+	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (new == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	while ((size = read(old, buf, 1024)) > 0)
 	{
