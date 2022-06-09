4206fdee62e4a0d6884ce5720deda509615bec66
Sharon Nderi <nderisharonw@gmail.com>
2022-06-09 17:32:50 +0000

    Correct return

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -51,6 +51,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = tmp;
 	new->next = temp;
 	tmp->next = new;
-	temp->prev = new;
+	if (temp)
+		temp->prev = new;
 	return (new);
 }
