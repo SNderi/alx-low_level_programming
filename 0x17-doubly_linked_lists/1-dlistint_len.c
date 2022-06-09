0a0e139f8400ec8c979cb173ae195ef5fd6dc745
Sharon Nderi <nderisharonw@gmail.com>
2022-06-09 15:15:02 +0000

    Betty style and variable new declaration

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - Counts the number of elements in a linked list
  *
  * @h: Pointer to head of list
- * Return - Number of elements
+ * Return: Number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
