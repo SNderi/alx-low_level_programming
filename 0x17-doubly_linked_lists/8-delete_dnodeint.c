af82f11a4c547f1c214d60a1c5cf8964949c33e5
SNderi <sharonscarlet8@gmail.com>
2022-06-09 21:15:13 +0300

    Added dlistint_len

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Counts the number of elements in a linked list
+ *
+ * @h: Pointer to head of list
+ * Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
  * delete_dnodeint_at_index - Deletes the node at a given position
  *
  * @head: Pointer to list's head
