550596afa56b8123343ec1d9c87ea5a91d551121
SNderi <sharonscarlet8@gmail.com>
2022-07-01 21:32:56 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,80 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * replace - replaces the value at a pre-existing key
- * @ht: double pointer to the hash_node_t list
- * @key: new key to add in the node
- * @value: value to add in the node
- */
-
-void replace(hash_node_t **ht, const char *key, const char *value)
-{
-	hash_node_t *temp = *ht;
-
-	while (temp && strcmp(temp->key, key))
-		temp = temp->next;
-
-	free(temp->value);
-	temp->value = strdup(value);
-}
-
-
-/**
- * validate - Checks if a key already exixts in a hash table
- * @ht: Pointer to the list
- * @key: Key to validate
- *
- * Return: 1 if key is found 0 otherwise
- */
-
-int validate(hash_node_t *ht, const char *key)
-{
-	while (ht)
-	{
-		if (!strcmp(ht->key, key))
-			return (1);
-		else
-			ht = ht->next;
-	}
-	return (0);
-}
-
-
-/**
- * add_node -  Adds a new node at the beginning of a linked list
- * @h: Pointer to the list
- * @key: key to add
- * @value: value associated with the key
- *
- * Return: address of new element if it succeeded, NULL otherwise
- */
-
-hash_node_t *add_node(hash_node_t *h, const char *key, const char *value)
-{
-	hash_node_t *nod;
-
-	nod = malloc(sizeof(hash_node_t));
-	if (!nod)
-		return (NULL);
-
-	nod->key = strdup(key);
-	nod->value = strdup(value);
-
-	if (h == NULL)
-	{
-		h = nod;
-		nod->next = NULL;
-	}
-	else
-	{
-		nod->next = h;
-		h = nod;
-	}
-
-	return (h);
-}
-
-/**
  * hash_table_set -  Adds an element to the hash table
  * @ht: Hash table to add or update the key/value to
  * @key: key
@@ -85,20 +11,43 @@ hash_node_t *add_node(hash_node_t *h, const char *key, const char *value)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (!ht || !key || !strcmp(key, "") || !value)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-	if (validate(ht->array[index], key))
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		replace(&ht->array[index], key, value);
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
-	add_node(ht->array[index], key, value);
-	if (ht->array[index] == NULL)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
 		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
