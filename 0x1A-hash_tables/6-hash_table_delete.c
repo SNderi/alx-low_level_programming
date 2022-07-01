84b21a77ab5741a3df8c99ce9ceb04ed1667d13c
SNderi <sharonscarlet8@gmail.com>
2022-07-01 22:08:41 +0300

    Free hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,25 +7,20 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+	unsigned int idx;
+	hash_node_t *list;
 
-	for (i = 0; i < ht->size; i++)
+	for (idx = 0; idx < ht->size; idx++)
 	{
-		if (ht->array[i] != NULL)
+		while (ht->array[idx] != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			list = ht->array[idx]->next;
+			free(ht->array[idx]->value);
+			free(ht->array[idx]->key);
+			free(ht->array[idx]);
+			ht->array[idx] = list;
 		}
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
