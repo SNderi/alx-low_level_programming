df3fc23d7a44a0a0222ae7da2b767c2c4ccb1ff7
Sharon Nderi <nderisharonw@gmail.com>
2022-07-01 18:39:30 +0000

    Added index check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,15 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node = NULL;
 
-	if (!ht || !key || !strcmp(key, ""))
+	if (!ht || !key || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
 	node = (ht->array)[index];
 
 	while (node)
 	{
-		if (!strcmp(node->key, (char *)key))
+		if (!strcmp(node->key, key))
 			return (node->value);
 		node = node->next;
 	}
