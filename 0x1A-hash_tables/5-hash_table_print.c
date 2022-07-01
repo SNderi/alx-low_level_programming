32936dd7c5b24fa4712d9709ee360084e7558b3d
SNderi <sharonscarlet8@gmail.com>
2022-07-01 21:53:47 +0300

    Added header file

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,20 +1,4 @@
 #include "hash_tables.h"
-
-/**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the hash_node_t list to print
- */
-void print_list(hash_node_t *h)
-{
-	while (h)
-	{
-		printf("'%s': '%s'", h->key, h->value);
-		if (h->next)
-			printf(", ");
-		h = h->next;
-	}
-}
-
 /**
  * hash_table_print - Prints a hash table
  * @ht: Hash table to print
@@ -22,11 +6,11 @@ void print_list(hash_node_t *h)
 
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *node;
 	unsigned long int i;
-	hash_node_t *node = NULL;
-	int comma = 0;
+	unsigned char comma_flag = 0;
 
-	if (!ht)
+	if (ht == NULL)
 		return;
 
 	printf("{");
@@ -34,17 +18,18 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] != NULL)
 		{
-			if (comma == 1)
+			if (comma_flag == 1)
 				printf(", ");
 
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				print_list(ht->array[i]);
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
 				if (node != NULL)
 					printf(", ");
 			}
-			comma = 1;
+			comma_flag = 1;
 		}
 	}
 	printf("}\n");
